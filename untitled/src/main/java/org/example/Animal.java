d92d5724f2e553fc1a0e016b4334fbc4e9cad041
kaitohaidang <137318767+kaitohaidang@users.noreply.github.com>
2026-05-11 14:15:33 +0700

    add

---
+++ untitled/src/main/java/org/example/Animal.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class Animal {
+    protected String name;
+    public void makeSound() {
+        System.out.printf("gau gau");
+    }
+}
---
+++ untitled/src/main/java/org/example/Dog.java
@@ -0,0 +1,8 @@
+package org.example;
+
+import javax.xml.namespace.QName;
+
+public class Dog extends Animal{
+    public Dog(String name) { this.name= name;
+    }
+}
